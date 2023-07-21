43424332094d9d78be5917de183df7e4ad25bf96
HernandoR <lzhen.dev@outlook.com>
2023-07-21 07:27:40 +0000

    need to adjust rsync opts

--- restore_and_backup.sh
+++ restore_and_backup.sh
@@ -1,5 +1,5 @@
 
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 # This is a shell script that provides functionality to backup, update, and restore dotfiles using
 # rsync. It also includes options for installing zsh and Oh My Zsh. The script uses command-line
@@ -7,14 +7,16 @@
 # post-processing logic.
 
 # Default values
-help=false
-quiet=false
-install_omz=false
+declare -g help=false
+declare -g quiet=false
+declare -g install_omz=false
+declare -g dry_run=false
 
 # get the current directory
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+declare -g DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 
+declare -g rsync_default_options="avh"
 
 #------------------------------------------------------------------------------
 
@@ -59,19 +61,23 @@ backup_dotfiles() {
     mkdir -p "$dest_dir"
 
     # Perform the backup using rsync
-	rsync --exclude ".git*" \
+	rsync \
+        --include "/.*" \
+        --include ".vim/*" \
+        --include ".tmux/*" \
+        --exclude ".git*" \
+        --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
         --exclude ".z" \
         --exclude ".zcompdump*" \
+        --exclude ".cache/" \
         --exclude "*history*" \
-        --include ".vim/*" \
-		--include ".tmux*" \
         --exclude "*/" \
-		-avuh --no-perms "$source_dir" "$dest_dir"
+        --exclude "*" \
+		-$rsync_default_options \
+        --update --no-perms \
+        "$source_dir" "$dest_dir"
 
     echo "Dotfiles backup complete!"
 }
@@ -95,40 +101,30 @@ restore_dotfiles() {
         --exclude ".zcompdump*" \
         --exclude "*history*" \
         --include ".vim/" \
-        --exclude "*/" \
-		-avh --no-perms "$backup_dir" "$restore_dir"
+        --include ".tmux*" \
+        --exclude "/"
+		-$rsync_default_options \
+        --no-perms \
+        "$backup_dir" "$restore_dir"
 
     echo "Dotfiles restore complete!"
 }
 
 
-
-# function doIt() {
-# 	rsync --exclude ".git/" \
-# 		--exclude ".DS_Store" \
-# 		--exclude ".osx" \
-# 		--exclude "bootstrap.sh" \
-# 		--exclude "README.md" \
-# 		--exclude "LICENSE-MIT.txt" \
-# 		-avh --no-perms ./sources/root ~;
-	
-# }
-
-
-# if [ $quiet ]; then
-# 	doIt;
-# else
-# 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-# 	echo "";
-# 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-# 		doIt;
-# 	fi;
-    
-# fi;
-
 # Function for backup subcommand
 do_backup() {
     echo "Performing backup..."
+    echo "opts $rsync_default_options"
+    if [ ! $quiet ];
+    then
+        echo "continue? y/n"
+        read -n 1 -r
+        if [[ $REPLY == [nN]$ ]]
+        then
+            exit 1
+        fi
+    fi
+
     # Add backup logic here
     pre_process;
     backup_dotfiles $HOME/ ./sources/root/ ;
@@ -165,80 +161,104 @@ post_process(){
 
 # function for help 
 usage(){
-    echo "havent implement yet"
+    echo "usage havent implement yet"
 }
 
 
 #------------------------------------------------------------------------------
 
 
+# Default values for options
+quiet=false
+verbose=false
+dry_run=false
 
-# Parsing command-line options
-while getopts ":hq-:" opt; do
-    case $opt in
-        h | -help )
-            help=true
+phase_args(){
+    # Args handding
+    while getopts ":hvqd-" opt; do # Go through the options
+
+        case $opt in
+            h ) # Help
+                usage
+                exit 0 # Exit correctly
+            ;;
+            v ) # Debug
+                echo "Read verbose flag"
+                verbose=true
+            ;;
+            q | -quiet )
+                quiet=true
+                ;;
+            d | -dry )
+                dry_run=true
+                ;;
+            - )
+                case "${OPTARG}" in
+                    help )
+                        usage
+                        exit 0
+                        ;;
+                    quiet )
+                        quiet=true
+                        ;;
+                    install-omz )
+                        install_omz=true
+                        ;;
+                    * )
+                        echo "Invalid option: --${OPTARG}"
+                        usage
+                        exit 1
+                        ;;
+                esac
+            ;;
+            : )
+                echo "Option requires an argument: -$OPTARG"
+                usage
+                exit 1
+                ;;
+            ? ) # Invalid option
+                echo "[ERROR]: Invalid option: -${OPTARG}"
+                usage
+                exit 1 # Exit with erro
+            ;;
+        esac
+    done
+    echo "dry_run: $dry_run"
+    if $dry_run; then
+        rsync_default_options+="n"
+    fi
+}
+
+if [ ! 0 == $# ] # If options provided then 
+then
+    subcommand=$1; shift # Get subcommand and shift to next option
+    # subcmd handling
+    case "$subcommand" in
+        backup )
+                unset OPTIND # in order to make -v pow -a <arg> -f <arg> work -> https://stackoverflow.com/questions/2189281/how-to-call-getopts-in-bash-multiple-times
+                if [ ! 0 == $# ] # if options provided
+                then
+                    if [ $verbose == true ]; then echo "Remaining args are: <${@}>"; fi
+                    phase_args "$@"
+                fi
+
+                do_backup
+            ;;
+        update )
+            do_update
+            ;;
+        restore )
+            do_restore
+            ;;
+        * ) # Invalid subcommand
+            if [ ! -z $subcommand ]; then  # Don't show if no subcommand provided
+                echo "Invalid subcommand: $subcommand"
+            fi
             usage
-            exit 0
-            ;;
-        q | -quiet )
-            quiet=true
-            ;;
-        d | -dry )
-            dry_run=true
-            ;;
-        - )
-            case "${OPTARG}" in
-                help )
-                    help=true
-                    usage
-                    exit 0
-                    ;;
-                quiet )
-                    quiet=true
-                    ;;
-                install-omz )
-                    install_omz=true
-                    ;;
-                * )
-                    echo "Invalid option: --${OPTARG}"
-                    usage
-                    exit 1
-                    ;;
-            esac
-            ;;
-        : )
-            echo "Option requires an argument: -$OPTARG"
-            usage
-            exit 1
-            ;;
-        \? )
-            echo "Invalid option: -$OPTARG"
-            usage
-            exit 1
-            ;;
+            exit 1 # Exit with error
+        ;;
     esac
-done
-
-# Main script logic
-if [[ $# -eq 0 ]]; then
+else # else if no options provided throw error
     usage
     exit 1
 fi
-
-case "$1" in
-    backup )
-        do_backup
-        ;;
-    update )
-        do_update
-        ;;
-    restore )
-        do_restore
-        ;;
-    * )
-        echo "Invalid subcommand: $1"
-        echo "Usage: script.sh [backup|update|restore]"
-        exit 1
-        ;;
-esac
