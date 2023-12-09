6b172a29c9be710369c272f1c738b22e54434838
HernandoR <lzhen.dev@outlook.com>
2023-12-09 22:39:18 +0800

    update brew

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,16 @@
 sudo apt update
-sudo apt -y remove libcurl4
-sudo apt -y install curl git zsh rsync
+if ! command -v curl &> /dev/null
+then
+    sudo apt -y remove libcurl4
+    sudo apt -y install curl
+fi
+
+sudo apt -y install git zsh rsync
 
 ./config-ohmyzsh.sh
 
 ./install-homebrew.sh
+
+
 
 ./restore_and_backup.sh restore
