173738293f53e32df2ffb2904c5246ef30144964
HernandoR <lzhen.dev@outlook.com>
2024-01-16 21:11:03 +0800

    add rsync to brew

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,31 @@
-sudo apt update
+if ! command -v brew &> /dev/null;
+then
+    echo "Installing Homebrew"
+    ./install-homebrew.sh
+fi
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt update
     if ! command -v curl &> /dev/null
     then
         sudo apt -y remove libcurl4
         sudo apt -y install curl
     fi
     sudo apt -y install git zsh rsync
+elif [[ "$OSTYPE" =="darwin"* ]]; then
+    # for macos devices, use brew
+    if ! command -v curl &> /dev/null
+    then
+        brew install curl
+    fi
+    brew install git zsh rsync
+else
+    echo "Unknown OS"
+    exit 1
 fi
 
 ./config-ohmyzsh.sh
 
-./install-homebrew.sh
 
 
 
