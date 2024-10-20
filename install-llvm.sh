3c27c879b74077517e2de8ef0034d04aa3a1b33f
HernandoR <lzhen.dev@outlook.com>
2024-10-20 22:02:02 +0800

    fix bad arg on wget

--- install-llvm.sh
+++ install-llvm.sh
@@ -1,6 +1,6 @@
 version=${1:-18}
 echo "Installing LLVM version $version"
-wget https://apt.llvm.org/llvm.sh -o ~/.local/bin/llvm.sh
+wget https://apt.llvm.org/llvm.sh -O ~/.local/bin/llvm.sh
 chmod +x ~/.local/bin/llvm.sh
 sudo ~/.local/bin/llvm.sh $@
 
