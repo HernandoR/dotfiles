10feb45dbe594bed45cd4fd73b43841aa4209348
liuzhen19 <liuzhen19@xiaomi.com>
2024-10-24 15:27:04 +0800

    update mode
    Signed-off-by: liuzhen19 <liuzhen19@xiaomi.com>

---
+++ install_llvm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+version=$1
+echo "Installing LLVM version $version"
+wget https://apt.llvm.org/llvm.sh -O ~/.local/bin/llvm.sh
+chmod +x ~/.local/bin/llvm.sh
+sudo ~/.local/bin/llvm.sh $@
+
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-$version 100 \
+                    --slave /usr/bin/clang++ clang++ /usr/bin/clang++-$version \
+                    --slave /usr/bin/ld lld /usr/bin/lld-$version \
+                    --slave /usr/bin/clang-cpp clang-cpp /usr/bin/clang-cpp-$version \
+                    --slave /usr/bin/clangd clangd /usr/bin/clangd-$version \
+                    --slave /usr/bin/clang-format clang-format /usr/bin/clang-format-$version \
+                    --slave /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-$version \
+                    --slave /usr/bin/clang-cl clang-cl /usr/bin/clang-cl-$version \
+                    --slave /usr/bin/clang-query clang-query /usr/bin/clang-query-$version \
+                    --slave /usr/bin/clang-rename clang-rename /usr/bin/clang-rename-$version
+for file in /usr/bin/*-"$version"; do
+    if [ -f "$file" ]; then
+        base_name=$(basename "$file" -"$version")
+        if [ ! -f "/usr/bin/$base_name" ]; then
+            sudo update-alternatives --install /usr/bin/"$base_name" "$base_name" /usr/bin/"${base_name}"-"$version" 1
+        fi
+    fi
+done
