52d8691c9319cb83fc2c12998eb8ffe436c0f9ce
liuzhen19 <liuzhen19@xiaomi.com>
2025-03-05 12:00:23 +0800

    feat(bootstrap): 更新脚本以支持不同操作系统的检测和安装
    添加对操作系统类型的检测，支持 Debian 和 macOS 的安装流程。
    修复了脚本中的条件判断错误，确保正确执行。
    
    - 增加了对 /etc/os-release 的读取
    - 修正了条件判断语法
    
    Signed-off-by: liuzhen19 <liuzhen19@xiaomi.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ fi
 OSTYPE=$(cat /etc/os-release |grep ID_LIKE | cut -d'=' -f2)
 echo "OS: $OSTYPE"
 
-if [[ "$OSTYPE" == "debian" ]]; then
+if [ "$OSTYPE" = "debian" ]; then
     sudo apt update
     if ! command -v curl &> /dev/null
     then
@@ -17,7 +17,7 @@ if [[ "$OSTYPE" == "debian" ]]; then
         sudo apt -y install xclip # for tmux clipboard
     fi
     sudo apt -y install git zsh rsync
-elif [[ "$OSTYPE" =="darwin"]]; then
+elif [ "$OSTYPE" = "darwin"]; then
     # for macos devices, use brew
     if ! command -v curl &> /dev/null
     then
